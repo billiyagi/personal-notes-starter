43e7375f12199f2f3ab3451523ca73cfd5d0e9cb
Febry Billiyagi <febrybilliyagi72@gmail.com>
2025-07-06 18:32:07 +0700

    refac: removing unused codes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NoteForm from "./components/NoteForm";
-import { getActiveNotes, getArchivedNotes } from "./utils";
+import { getActiveNotes } from "./utils";
 import NoteItem from "./components/NoteItem";
 
 class App extends React.Component {
--- src/components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SearchBar() {
-	return (
-		<>
-			<input type="text" placeholder="search your note..." />
-		</>
-	);
-}
